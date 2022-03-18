6cff4c1b3a944700db463dec93690ac2ba9e7170
Zane Dunn <60718015+dunndeft@users.noreply.github.com>
2022-03-18 13:37:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+// console.log(secretMessage.length); // 24 elements/strings in the array.
+
+secretMessage.pop();
+// console.log(secretMessage);
+// console.log(secretMessage.length); // 23 elements/strings in the array.
+
+secretMessage.push('to', 'Program');
+// console.log(secretMessage);
+
+secretMessage[7] = 'right';
+// console.log(secretMessage);
+
+secretMessage.shift();
+// console.log(secretMessage);
+
+secretMessage.unshift('Programming');
+// console.log(secretMessage);
+
+secretMessage.splice(6, 5, 'know');
+// console.log(secretMessage);
+
+console.log(secretMessage.join(' '));
